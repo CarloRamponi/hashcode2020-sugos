867339090d309d6db7cba84db3f531c90c9e0b78
Nicola Salsotto <nicolasalsotto@gmail.com>
2020-02-20 20:44:12 +0100

    Update script

--- script.sh
+++ script.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-cat "a_example.txt" | ./a.out > a.output
-echo "PID = $!"
+cat "a_example.txt" | ./a.out > a.output &
+A=$!
+echo "PID A = $A"
 
-cat "b_read_on.txt" | ./a.out > b.output
-echo "PID = $!"
+cat "b_read_on.txt" | ./a.out > b.output &
+B=$!
+echo "PID B = $B"
 
-cat "c_incunabula.txt" | ./a.out > a.output
-echo "PID = $!"
+cat "c_incunabula.txt" | ./a.out > c.output &
+C=$!
+echo "PID C = $C"
 
-cat "d_tough_choices.txt" | ./a.out > a.output
-echo "PID = $!"
+cat "d_tough_choices.txt" | ./a.out > d.output &
+D=$!
+echo "PID D = $D"
 
-cat "e_so_many_books.txt" | ./a.out > a.output
-echo "PID = $!"
+cat "e_so_many_books.txt" | ./a.out > e.output &
+E=$!
+echo "PID E = $E"
 
-cat "f_libraries_of_the_world.txt" | ./a.out > a.output
-echo "PID = $!"
+cat "f_libraries_of_the_world.txt" | ./a.out > f.output &
+F=$!
+echo "PID F = $F"
+
+
+wait $A
+wait $B
+wait $C
+wait $D
+wait $E
+wait $F
+
+notify-send "Operazione terminata"
